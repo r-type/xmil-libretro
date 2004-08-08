5f05b4595ed22f49a5f3b7d9679921e70c63febc
yui <yui@afe8d728-ceca-4c4c-a723-f8cfeda826dd>
2004-08-08 12:32:14 +0000

    add...

---
+++ macos9/compiler.h
@@ -0,0 +1,67 @@
+#include	<string.h>
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	<stdarg.h>
+#include	<stddef.h>
+#if defined(CPUCORE_IA32)
+#include	<setjmp.h>
+#endif
+
+#define	MACOS
+#define	BYTESEX_BIG
+#define	OSLANG_SJIS
+#define	OSLINEBREAK_CR
+
+
+typedef signed int		SINT;
+typedef unsigned int	UINT;
+typedef signed char		SINT8;
+typedef unsigned char	UINT8;
+typedef signed short	SINT16;
+typedef unsigned short	UINT16;
+typedef signed int		SINT32;
+typedef unsigned int	UINT32;
+typedef SInt64			SINT64;
+typedef UInt64			UINT64;
+typedef Boolean			BOOL;
+
+
+typedef unsigned char	BYTE;
+typedef unsigned short	WORD;
+typedef unsigned int	DWORD;
+
+
+#define	MAX_PATH	256
+
+#define	ZeroMemory(a, b)		memset((a),  0 , (b))
+#define	FillMemory(a, b, c)		memset((a), (c), (b))
+#define	CopyMemory(a, b, c)		memcpy((a), (b), (c))
+
+#define	max(a, b)				(((a)>(b))?(a):(b))
+#define	min(a, b)				(((a)<(b))?(a):(b))
+
+
+#define	BRESULT				Boolean
+#define	OEMCHAR				char
+#define	OEMTEXT(string)		(string)
+
+#include	"common.h"
+#include	"macossub.h"
+#include	"milstr.h"
+#include	"_memory.h"
+#include	"trace.h"
+#include	"rect.h"
+
+
+#define	GETTICK()			macos_gettick()
+#define	SPRINTF				sprintf
+#define	STRLEN				strlen
+#define	__ASSERT(s)
+
+#define	SUPPORT_SJIS
+
+#define	SUPPORT_32BPP
+#define	MEMOPTIMIZE		1
+
+#define	SOUNDRESERVE	80
+
