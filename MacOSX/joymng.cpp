44a29cdb7b28a4ce1cfb64ba24b15a52901ed405
tk800 <tk800@afe8d728-ceca-4c4c-a723-f8cfeda826dd>
2004-08-10 14:38:57 +0000

    Add MacOSX[MacOSX](tk800)

---
+++ MacOSX/joymng.cpp
@@ -0,0 +1,9 @@
+#include	"compiler.h"
+#include	"joymng.h"
+
+
+REG8 joymng_getstat(void) {
+
+	return(0xff);
+}
+
---
+++ MacOSX/menu.h
@@ -0,0 +1,20 @@
+
+void menu_setiplrom(UINT8 value);
+void menu_setbootmedia(UINT8 value);
+void menu_setresolute(UINT8 value);
+void menu_setwidth(UINT8 value);
+void menu_setdispmode(UINT8 value);
+void menu_setraster(UINT8 value);
+void menu_setwaitflg(UINT8 value);
+void menu_setframe(UINT8 value);
+void menu_setkey(UINT8 value);
+void menu_setsound(UINT8 value);
+void menu_setmouse(UINT8 value);
+void menu_setcpuspeed(UINT8 value);
+void menu_setmotorflg(UINT8 value);
+void menu_setdispclk(UINT8 value);
+void menu_setskipline(UINT8 value);
+void menu_setbtnmode(UINT8 value);
+void menu_setbtnrapid(UINT8 value);
+void menu_setmsrapid(UINT8 value);
+
