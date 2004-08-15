0b393dfd0069992443aeedd9f16df969d1f0a196
yui <yui@afe8d728-ceca-4c4c-a723-f8cfeda826dd>
2004-08-15 11:25:33 +0000

    fix...

--- xmil/trunk/fdd/diskdrv.c
+++ xmil/trunk/fdd/diskdrv.c
@@ -15,5 +15,15 @@ void diskdrv_setfdd(REG8 drv, const OEMCHAR *fname, int readonly) {
 	sysmng_update(SYS_UPDATEFDD);
 }
 
+void diskdrv_setfddex(REG8 drv, const OEMCHAR *fname,
+												UINT ftype, int readonly) {
+
+	fddfile_eject(drv);
+	if (fname) {
+		fddfile_set(drv, fname, ftype, readonly);
+	}
+	sysmng_update(SYS_UPDATEFDD);
+}
+
 // void diskdrv_callback(void) { }
 
--- xmil/trunk/fdd/diskdrv.h
+++ xmil/trunk/fdd/diskdrv.h
@@ -4,6 +4,8 @@ extern "C" {
 #endif
 
 void diskdrv_setfdd(REG8 drv, const OEMCHAR *fname, int readonly);
+void diskdrv_setfddex(REG8 drv, const OEMCHAR *fname,
+												UINT ftype, int readonly);
 // void diskdrv_callback(void);
 
 #ifdef __cplusplus
