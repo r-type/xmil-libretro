d80674fbb55fb3fbe9c98e419e3f00e591e64fd0
yui <yui@afe8d728-ceca-4c4c-a723-f8cfeda826dd>
2014-06-11 14:49:09 +0000

    ♳ SDL2: supports joystick temporarily

--- xmil/branches/yui/WORK_01/sdl2/joymng.h
+++ xmil/branches/yui/WORK_01/sdl2/joymng.h
@@ -1,11 +1,18 @@
 
+#pragma once
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-BYTE joymng_getstat(void);
+REG8 joymng_getstat(void);
 
 #ifdef __cplusplus
 }
 #endif
 
+void joymng_initialize(void);
+void joymng_deinitialize(void);
+void joymng_axismotion(const SDL_Event *event);
+void joymng_buttondown(const SDL_Event *event);
+void joymng_buttonup(const SDL_Event *event);
