9491fb275a1d8216afd5a2f6395ce177a4e098be
yui <yui@afe8d728-ceca-4c4c-a723-f8cfeda826dd>
2014-06-13 08:51:16 +0000

    ♳ SDL2: revert joysticks  svn merge -r 132:131 https://amethyst.yui.ne.jp/svn-dev/x1/xmil/branches/yui/WORK_01

--- WORK_01/sdl2/joymng.h
+++ WORK_01/sdl2/joymng.h
@@ -1,18 +1,11 @@
 
-#pragma once
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-REG8 joymng_getstat(void);
+BYTE joymng_getstat(void);
 
 #ifdef __cplusplus
 }
 #endif
 
-void joymng_initialize(void);
-void joymng_deinitialize(void);
-void joymng_axismotion(const SDL_Event *event);
-void joymng_buttondown(const SDL_Event *event);
-void joymng_buttonup(const SDL_Event *event);
