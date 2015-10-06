3de7849be9bf85ddc204f9a7f4415162261cd9a3
yui <yui@afe8d728-ceca-4c4c-a723-f8cfeda826dd>
2015-10-06 10:07:41 +0000

    ✓ SDL2: warning (XCode)

--- xmil/branches/yui/WORK_01/sdl2/iOS/main.m
+++ xmil/branches/yui/WORK_01/sdl2/iOS/main.m
@@ -8,12 +8,8 @@
 
 int SDL_main(int argc, char *argv[])
 {
-	NSArray *paths;
-	NSString *DocumentsDirPath;
-	char path[MAX_PATH];
-
-	paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-	DocumentsDirPath = [paths objectAtIndex:0];
+	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+	NSString *DocumentsDirPath = [paths objectAtIndex:0];
 
 	NSString *current = [DocumentsDirPath stringByAppendingString:@"/"];
 	file_setcd([current UTF8String]);
