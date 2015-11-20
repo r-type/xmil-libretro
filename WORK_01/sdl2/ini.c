e3a4119d10fb2ef1a8b9207f386239d7e1194f58
yui <yui@afe8d728-ceca-4c4c-a723-f8cfeda826dd>
2015-11-20 13:32:19 +0000

    ✓ sdl2: SUPPORT_OPM

--- WORK_01/sdl2/ini.c
+++ WORK_01/sdl2/ini.c
@@ -35,9 +35,9 @@ static const PFTBL iniitem[] = {
 	{"Seek_Snd", PFTYPE_BOOL,		&xmilcfg.MOTOR,			0},
 	{"Seek_Vol", PFMAX_UINT8,		&xmilcfg.MOTORVOL,		100},
 #endif	/* !defined(DISABLE_SOUND) */
-#if defined(SUPPORT_OPM)
+#if defined(SUPPORT_TURBOZ) || defined(SUPPORT_OPM)
 	{"OPMsound", PFTYPE_BOOL,		&xmilcfg.SOUND_SW,		0},
-#endif	//defined(SUPPORT_OPM)
+#endif	/* defined(SUPPORT_TURBOZ) || defined(SUPPORT_OPM) */
 
 	{"MouseInt", PFTYPE_BOOL,		&xmilcfg.MOUSE_SW,		0},
 	{"btnRAPID", PFTYPE_BOOL,		&xmilcfg.BTN_RAPID,		0},
