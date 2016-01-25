58f1afcccdd98fb1412fe71a576ae836fa641d36
yui <yui@afe8d728-ceca-4c4c-a723-f8cfeda826dd>
2016-01-25 12:55:57 +0000

    ↺ merge  svn merge -r 349:369 https://amethyst.yui.ne.jp/svn-dev/x1/xmil/trunk

--- xmil/branches/yui/WORK_02/win9x/ext/externalopna.cpp
+++ xmil/branches/yui/WORK_02/win9x/ext/externalopna.cpp
@@ -13,23 +13,38 @@
 CExternalOpna::CExternalOpna(IExternalChip* pChip)
 	: CExternalPsg(pChip)
 	, m_bHasPsg(false)
+	, m_bHasExtend(false)
 	, m_bHasRhythm(false)
 	, m_bHasADPCM(false)
+	, m_cMode(0)
 {
 	memset(m_cAlgorithm, 0, sizeof(m_cAlgorithm));
 	memset(m_cTtl, 0x7f, sizeof(m_cTtl));
 
 	switch (GetChipType())
 	{
+		case IExternalChip::kYM2203:
+			m_bHasPsg = true;
+			break;
+
 		case IExternalChip::kYM2608:
 			m_bHasPsg = true;
+			m_bHasExtend = true;
 			m_bHasRhythm = true;
 			m_bHasADPCM = true;
 			break;
 
+		case IExternalChip::kYM3438:
+			m_bHasExtend = true;
+			break;
+
 		case IExternalChip::kYMF288:
 			m_bHasPsg = true;
+			m_bHasExtend = true;
 			m_bHasRhythm = true;
+			break;
+
+		default:
 			break;
 	}
 }
@@ -46,9 +61,17 @@ CExternalOpna::~CExternalOpna()
  */
 void CExternalOpna::Reset()
 {
+	m_cMode = 0;
 	memset(m_cAlgorithm, 0, sizeof(m_cAlgorithm));
 	memset(m_cTtl, 0x7f, sizeof(m_cTtl));
-	CExternalPsg::Reset();
+	if (m_bHasPsg)
+	{
+		CExternalPsg::Reset();
+	}
+	else
+	{
+		m_pChip->Reset();
+	}
 }
 
 /**
@@ -60,11 +83,23 @@ void CExternalOpna::WriteRegister(UINT nAddr, UINT8 cData)
 {
 	if (nAddr < 0x10)
 	{
-		CExternalPsg::WriteRegister(nAddr, cData);
+		if (m_bHasPsg)
+		{
+			CExternalPsg::WriteRegister(nAddr, cData);
+		}
 	}
 	else
 	{
-		if ((nAddr & 0xf0) == 0x40)
+		if (nAddr == 0x27)
+		{
+			cData &= 0xc0;
+			if (m_cMode == cData)
+			{
+				return;
+			}
+			m_cMode = cData;
+		}
+		else if ((nAddr & 0xf0) == 0x40)
 		{
 			// ttl
 			m_cTtl[((nAddr & 0x100) >> 4) + (nAddr & 15)] = cData;
@@ -84,13 +119,19 @@ void CExternalOpna::WriteRegister(UINT nAddr, UINT8 cData)
  */
 void CExternalOpna::Mute(bool bMute) const
 {
-	CExternalPsg::Mute(bMute);
+	if (m_bHasPsg)
+	{
+		CExternalPsg::Mute(bMute);
+	}
 
 	const int nVolume = (bMute) ? -127 : 0;
 	for (UINT ch = 0; ch < 3; ch++)
 	{
 		SetVolume(ch + 0, nVolume);
-		SetVolume(ch + 4, nVolume);
+		if (m_bHasExtend)
+		{
+			SetVolume(ch + 4, nVolume);
+		}
 	}
 }
 
@@ -103,7 +144,7 @@ void CExternalOpna::SetVolume(UINT nChannel, int nVolume) const
 {
 	const UINT nBaseReg = (nChannel & 4) ? 0x140 : 0x40;
 
-	//! アルゴリズム スロット マスク
+	/*! アルゴリズム スロット マスク */
 	static const UINT8 s_opmask[] = {0x08, 0x08, 0x08, 0x08, 0x0c, 0x0e, 0x0e, 0x0f};
 	UINT8 cMask = s_opmask[m_cAlgorithm[nChannel & 7] & 7];
 	const UINT8* pTtl = m_cTtl + ((nChannel & 4) << 2);
--- xmil/branches/yui/WORK_02/win9x/scrnmng.h
+++ xmil/branches/yui/WORK_02/win9x/scrnmng.h
@@ -31,6 +31,16 @@ enum {
 	SCRNFLAG_ENABLE			= 0x80
 };
 
+enum {
+	FSCRNMOD_NORESIZE		= 0x00,
+	FSCRNMOD_ASPECTFIX8		= 0x01,
+	FSCRNMOD_ASPECTFIX		= 0x02,
+	FSCRNMOD_LARGE			= 0x03,
+	FSCRNMOD_ASPECTMASK		= 0x03,
+	FSCRNMOD_SAMERES		= 0x04,
+	FSCRNMOD_SAMEBPP		= 0x08
+};
+
 typedef struct {
 	UINT8	flag;
 	UINT8	bpp;
@@ -66,15 +76,22 @@ RGB16 scrnmng_makepal16(RGB32 pal32);			// pal_get16pal
 
 // ---- for windows
 
+void scrnmng_setmultiple(int multiple);
+int scrnmng_getmultiple(void);
+
 void scrnmng_initialize(void);						// ddraws_initwindowsize
 BRESULT scrnmng_create(UINT8 scrnmode);				// ddraws_InitDirectDraw
 void scrnmng_destroy(void);							// ddraws_TermDirectDraw
-#if defined(SUPPORT_DCLOCK)
-void scrnmng_dispclock(void);						// ddraws_dispclock
-#endif	// defined(SUPPORT_DCLOCK)
 
 void scrnmng_querypalette(void);					// ddraws_palette
 void scrnmng_fullscrnmenu(int y);
 void scrnmng_topwinui(void);						// ddraws_topwinui
 void scrnmng_clearwinui(void);						// ddraws_clearwinui
 
+void scrnmng_entersizing(void);
+void scrnmng_sizing(UINT side, RECT *rect);
+void scrnmng_exitsizing(void);
+
+#if defined(SUPPORT_DCLOCK)
+void scrnmng_dispclock(void);
+#endif	// defined(SUPPORT_DCLOCK)
