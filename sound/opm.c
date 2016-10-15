8c5f889fe22889550b01940bf470ea646e62fba6
yui <yui@afe8d728-ceca-4c4c-a723-f8cfeda826dd>
2016-10-15 14:30:51 +0000

    opm: initializer refs #164  svn merge -r 368:374 https://amethyst.yui.ne.jp/svn-dev/x1/xmil/branches/yui/WORK_01

--- sound/opm.c
+++ sound/opm.c
@@ -36,7 +36,8 @@ void opm_reset(POPM opm, REG8 cCaps)
 {
 	memset(&opm->s, 0, sizeof(opm->s));
 	opm->s.cCaps = cCaps;
-	memset(&opm->s.reg[0x20], 0xff, 0xe0);
+	memset(&opm->s.reg[0x20], 0xc0, 0x08);
+	memset(&opm->s.reg[0x40], 0xff, 0xc0);
 
 	opmgen_reset(&opm->opmgen);
 }
